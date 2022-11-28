9bad67b8825ee7cdd2d2a7ea6ffe927b9f09219d
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-28 18:05:15 -0500

    renderGame function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let firstCard = 10
-let seconedCard = 11
+let seconedCard = 10
 let hasBlackJack = false
 let isAlive = true
 let message = ""
@@ -8,8 +8,11 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
 
+function startGame(){
+  renderGame()
+}
 
-function startGame(){ 
+function renderGame(){ 
     cardEl.textContent = `Card: ${firstCard} ${seconedCard}`
     sumEl.textContent = `Sum: ${sum}`
 if (sum < 21){
@@ -24,6 +27,9 @@ isAlive= false
 messageEl.textContent = message
 }
 function newCard(){
+  let card = 3
+    sum += card 
+   renderGame()
  console.log("Drawing a new card from the deck!")
 }
 // CASH OUT!
