bcf444916589f508adde5ddc3d8ec74d7a8bce0b
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-27 23:28:48 -0500

    index.js sumEl created

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 let firstCard = 10
-let seconedCard = 30
+let seconedCard = 11
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let sum = firstCard + seconedCard 
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
+console.log(messageEl)
 function startGame(){ 
+    sumEl.textContent = `Sum: ${sum}`
 if (sum < 21){
-    messageEl.textContent = "Do you want to draw a new card? 🙂"
+   message = "Do you want to draw a new card?"
 }else if(sum === 21){
-    messageEl.textContent = "Wohoo! You've got a blackJack 🥳"
+  message = "You've got a blackJack. "
 hasBlackJack  = true
 }else{
-    messageEl.textContent = "You're out of the game 😭"
+    message = "You're out of the game."
 isAlive= false
 }
-
+messageEl.textContent = message
 }
 // CASH OUT!
