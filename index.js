48638968b86df37054c12f5ca6020345f05c5c94
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-27 21:49:41 -0500

    index.js if statements added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let firstCard = 2
-let seconedCard = 11 
+let firstCard = 10
+let seconedCard = 21 
+let hasBlackJack = false
+let isAlive = true
 
 let sum = firstCard + seconedCard
 
@@ -7,6 +9,11 @@ if (sum < 21){
 console.log("Do you want to draw a new card? 🙂")
 }else if(sum === 21){
 console.log("Wohoo! You've got a blackJack 🥳")
+hasBlackJack  = true
 }else{
 console.log("You're out of the game 😭")
+isAlive= false
 }
+// CASH OUT!
+console.log(hasBlackJack)
+console.log(isAlive)
