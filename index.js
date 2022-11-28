3611286e4cec62d67df5cb63a10810b2f11d3eb3
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-27 21:18:16 -0500

    index.js starter code

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstCard = 2
+let seconedCard = 11 
+
+let sum = firstCard + seconedCard
+
+if (sum < 21){
+console.log("Do you want to draw a new card? 🙂")
+}else if(sum === 21){
+console.log("Wohoo! You've got a blackJack 🥳")
+}else{
+console.log("You're out of the game 😭")
+}
